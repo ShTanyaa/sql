fffac2d74a21078b0a0097e767071e71ea12902c
ShTanyaa <124123694+ShTanyaa@users.noreply.github.com>
2023-05-25 11:49:21 +0500

    Add files via upload

---
+++ WorkDM_shumkova/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace WorkDM_shumkova
+{
+    partial class Authorization_frm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Log_Txt = new System.Windows.Forms.TextBox();
+            this.Pass_txt = new System.Windows.Forms.TextBox();
+            this.Input_btn = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(17, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(17, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "пароль";
+            // 
+            // Log_Txt
+            // 
+            this.Log_Txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Log_Txt.Location = new System.Drawing.Point(101, 13);
+            this.Log_Txt.Name = "Log_Txt";
+            this.Log_Txt.Size = new System.Drawing.Size(201, 29);
+            this.Log_Txt.TabIndex = 2;
+            // 
+            // Pass_txt
+            // 
+            this.Pass_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Pass_txt.Location = new System.Drawing.Point(101, 71);
+            this.Pass_txt.Name = "Pass_txt";
+            this.Pass_txt.PasswordChar = '*';
+            this.Pass_txt.Size = new System.Drawing.Size(201, 29);
+            this.Pass_txt.TabIndex = 3;
+            // 
+            // Input_btn
+            // 
+            this.Input_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Input_btn.Location = new System.Drawing.Point(59, 132);
+            this.Input_btn.Name = "Input_btn";
+            this.Input_btn.Size = new System.Drawing.Size(234, 36);
+            this.Input_btn.TabIndex = 4;
+            this.Input_btn.Text = "войти в приложение";
+            this.Input_btn.UseVisualStyleBackColor = true;
+            this.Input_btn.Click += new System.EventHandler(this.Input_btn_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(435, 248);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.Pass_txt);
+            this.tabPage1.Controls.Add(this.Input_btn);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.Log_Txt);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(427, 222);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Вход";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(427, 222);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Регистрация";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(185, 110);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(201, 29);
+            this.textBox3.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(22, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 24);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "подтверждение";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(185, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(201, 29);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(185, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 29);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(74, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 36);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(22, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "пароль";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(22, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "логин";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 280);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(502, 140);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Authorization_frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 470);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Authorization_frm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Для входа введите логин и пароль";
+            this.Load += new System.EventHandler(this.Authorization_frm_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Log_Txt;
+        private System.Windows.Forms.TextBox Pass_txt;
+        private System.Windows.Forms.Button Input_btn;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WorkDM_shumkova/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkDM_shumkova
+{
+
+    public partial class Authorization_frm : Form
+    {
+        int count = 1;
+
+        public Authorization_frm()
+        {
+            InitializeComponent();
+        }
+       
+        private bool log(string login)
+        {
+            string zapros = $"select top(1000) \r\n [Login]\r\n  \r\n from[SecuretyDB_shumkova].[dbo].[User_tbl] where [Login]='{login}'";
+            SqlConnection connect = new SqlConnection(@"Data Source=PC325L11\SQLEXPRESS;Initial Catalog=SecuretyDB_shumkova;Integrated Security=True");
+            connect.Open( );
+            SqlDataAdapter adptr = new SqlDataAdapter("select * from User_tbl", connect);
+            DataTable table = new DataTable( );
+            adptr.Fill(table);
+            connect.Close( );
+            if ( table.Rows.Count > 0 ) return false;
+            else return true;
+        }
+        private bool pass (string password)
+        {
+            string zapros = $"select top(1000) \r\n [Password]\r\n  \r\n from[SecuretyDB_shumkova].[dbo].[User_tbl] where [Password]='{password}'";
+            SqlConnection connect = new SqlConnection(@"Data Source=PC325L11\SQLEXPRESS;Initial Catalog=SecuretyDB_shumkova;Integrated Security=True");
+            connect.Open( );
+            SqlDataAdapter adptr = new SqlDataAdapter("select * from User_tbl", connect);
+            DataTable table = new DataTable( );
+            adptr.Fill(table);
+            connect.Close( );
+            if ( table.Rows.Count > 0 ) return false;
+            else return true;
+        }
+
+        public void set_tbl()
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=PC325L11\SQLEXPRESS;Initial Catalog=SecuretyDB_shumkova;Integrated Security=True");
+            connect.Open();
+            SqlDataAdapter adptr = new SqlDataAdapter("select * from User_tbl", connect);
+            DataTable table = new DataTable();
+            adptr.Fill(table);
+            dataGridView1.DataSource = table;
+            connect.Close();
+        }
+
+
+        private void Input_btn_Click (object sender, EventArgs e)
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=PC325L11\SQLEXPRESS;Initial Catalog=SecuretyDB_shumkova;Integrated Security=True");
+
+            string log = Log_Txt.Text;
+            string pas = Pass_txt.Text;
+            if ( log == "" || pas == "" )
+            {
+                MessageBox.Show("заполните все поля", "error");
+            }
+            else
+            {
+                connect.Open( );
+                string prov = $"select Role,Active,Login from [dbo].[User_tbl] where Login='{log}' and password='{pas}'";
+                string s = $"update dbo.User_tbl set count+=1 where Login='{log}'";
+                string slct = $"select Login,active From dbo.User_tbl where Login='{log}' and Active='False'";
+
+
+                SqlDataAdapter adptr = new SqlDataAdapter(slct, connect);
+                DataTable table = new DataTable( );
+                adptr.Fill(table);
+                if ( table.Rows.Count == 1 )
+                {
+                    MessageBox.Show("пользователь заблокирован", "error");
+                    connect.Close( );
+                }
+                else
+                {
+                    SqlDataAdapter adptr1 = new SqlDataAdapter(prov, connect);
+                    adptr1.Fill(table);
+                    if ( table.Rows.Count == 0 )
+                    {
+                        MessageBox.Show("вы ввели неправильный пароль или логин", "error");
+                        SqlDataAdapter adptr2 = new SqlDataAdapter(s, connect);
+                        adptr2.Fill(table);
+                        string sq = $"update dbo.User_tbl set Active='False' where count>=3";
+                        SqlDataAdapter adptr3 = new SqlDataAdapter(sq, connect);
+                        adptr3.Fill(table);
+                        connect.Close( );
+
+                    }
+                    else
+                    {
+                        if ( table.Rows [ 0 ] [ 0 ].ToString( ) == "User" )
+                        {
+                             Root_frm f1 = new Root_frm( );
+                             f1.Show( );
+                             this.Hide( );
+                        }
+                        if ( table.Rows [ 0 ] [ 0 ].ToString( ) == "Admin" )
+                        {
+                            Admin_frm f2 = new Admin_frm( );
+                            f2.Show( );
+                            this.Hide( );
+                        }
+                        connect.Close( );
+                    }
+                }
+            }
+        }
+
+        private void Authorization_frm_Load(object sender, EventArgs e)
+        {
+            set_tbl();
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            if ( textBox2.Text.Length == 0 ) MessageBox.Show("введите логин", "error");
+            else if ( textBox1.Text.Length == 0 ) MessageBox.Show("введите пароль ", "error");
+            else if ( textBox1.Text != textBox3.Text ) MessageBox.Show("пароль не совпадает", "error");
+            else if ( log(textBox2.Text) == false ) MessageBox.Show("такой логин уже существует", "error");
+            else
+            {
+                string query = $"insert into dbo.User_tbl(login,Password,Count,Date,Active,Role) values ('{textBox2.Text}','{textBox1.Text}',0,{DateTime.Today.Year}-{DateTime.Today.Month}-{DateTime.Today.Day}','True','user')";
+                SqlConnection connect = new SqlConnection(@"Data Source=PC325L11\SQLEXPRESS;Initial Catalog=SecuretyDB_shumkova;Integrated Security=True");
+                connect.Open( );
+                SqlDataAdapter adptr = new SqlDataAdapter("select * from User_tbl", connect);
+                DataTable table = new DataTable( );
+                adptr.Fill(table);
+                connect.Close( );
+                set_tbl( );
+            }
+        }
+    }
+}
